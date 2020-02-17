b70e21f7cbd2e0f1884c6b3d6339120268b6aa2a
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-17 18:21:13 +0100

    Added ES index update

--- kaskadi-webhooks-status-lambda.js
+++ kaskadi-webhooks-status-lambda.js
@@ -1,11 +1,30 @@
+const es = require('aws-es-client')({
+  id: process.env.ES_ACCESS_ID,
+  token: process.env.ES_ACCESS_SECRET,
+  url: 'https://search-kaskadi-cl2e6mhgx3zc7ay2e5kkhjet4u.eu-central-1.es.amazonaws.com'
+})
+
+
 module.exports.handler = async (event) => {
+  console.log(event.body)
+  const eventBody = JSON.parse(event.body)
+  const body = {
+    kaskadiMeta: {
+      orderStatus: eventBody.eventData.statusName
+    }
+  }
+  es.update({
+    id: eventBody.eventData.externalId,
+    index: 'ysws-orders',
+    body
+  })
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      message: 'Your lambda is running!'
+      message: 'Webhook data received!'
     })
   }
 }
