72113d3eae7955ade44978aa39151e57344d0aee
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-05 14:41:15 +0100

    Added error logging

--- kaskadi-webhooks-status-lambda.js
+++ kaskadi-webhooks-status-lambda.js
@@ -18,6 +18,13 @@ module.exports.handler = async (event) => {
     })
   }
   if (eventBody.token !== token) {
+    const logData = {
+      error_type: 'unauthorized',
+      origin: event.headers.origin,
+      origin_country: event.headers['CloudFront-Viewer-Country']
+    }
+    console.log(logData)
+    await esLog(logData, 'error')
     response.statusCode = 401
     response.body = JSON.stringify({
       message: 'Unauthorized.'
@@ -25,12 +32,12 @@ module.exports.handler = async (event) => {
     return response
   }
   console.log(event.body)
-  await esLog(eventBody)
+  await esLog(eventBody, 'status')
   await updateOrderStatus(eventBody)
   return response
 }
 
-async function esLog(eventBody) {
+async function esLog(data, type) {
   const timestamp = Date.now()
   const timestampHex = timestamp.toString(16)
   const logDocId = `${timestampHex[0]}-${timestampHex.substr(1, 5)}-${timestampHex.substr(5, 5)}`
@@ -38,9 +45,9 @@ async function esLog(eventBody) {
     id: logDocId,
     index: 'ysws-logs',
     body: {
-      type: 'status',
+      type,
       date: timestamp,
-      ...eventBody
+      ...data
     }
   })
 }
