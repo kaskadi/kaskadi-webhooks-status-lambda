08b49b32bfdbadd2bc20492ca7b4e65b1c24e363
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-10 13:38:03 +0200

    Updated environment variables

--- kaskadi-webhooks-status-lambda.js
+++ kaskadi-webhooks-status-lambda.js
@@ -1,6 +1,6 @@
 const es = require('aws-es-client')({
-  id: process.env.ES_ACCESS_ID,
-  token: process.env.ES_ACCESS_SECRET,
+  id: process.env.ES_ID,
+  token: process.env.ES_SECRET,
   url: 'https://search-kaskadi-cl2e6mhgx3zc7ay2e5kkhjet4u.eu-central-1.es.amazonaws.com'
 })
 
